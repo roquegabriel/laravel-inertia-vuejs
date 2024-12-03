b7f888ee9f9dca4d635dd5503ff423ff92813171
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-12-03 13:07:27 -0300

    Add Ziggy

---
+++ resources/js/Layouts/Main.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <header class="bg-gray-800 text-white h-16 flex justify-start items-center">
+        <nav class="space-x-5">
+            <Link href="/" class="text-3xl">Home</Link>
+            <Link :href="route('about')" class="text-3xl">About</Link>
+        </nav>
+    </header>
+    <main>
+        <slot />
+    </main>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,26 @@
 import './bootstrap';
-
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { ZiggyVue } from 'ziggy-js';
+import Layout from "./Layouts/Main.vue";
 
 createInertiaApp({
+  title: title => `${title} | My App`,
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
+    let page = pages[`./Pages/${name}.vue`]
+    page.default.layout = page.default.layout || Layout
+    return page
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(ZiggyVue)
       .mount(el)
+  },
+  progress: {
+    // Whether the NProgress spinner will be shown...
+    showSpinner: true,
+    delay:250,
   },
 })
