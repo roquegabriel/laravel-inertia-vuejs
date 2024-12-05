9abc03862877fc4c6542615320bb3a64f5c9caa5
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-12-05 12:59:29 -0300

    Add dark mode

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -1,15 +1,20 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import { switchTheme } from '../theme';
 </script>
 
 <template>
-    <header class="bg-gray-800 text-white h-16 flex justify-start items-center">
-        <nav class="space-x-5">
-            <Link href="/" class="text-3xl">Home</Link>
-            <Link :href="route('about')" class="text-3xl">About</Link>
+    <header class="bg-slate-800 text-white">
+        <nav class="p-6 mx-auto max-w-screen-lg space-x-5 flex justify-between items-center">
+            <Link  href="/" class="text-3xl" :class="{ 'bg-gray-900 px-2 py-1 rounded-md': $page.component==='Home' }">Home</Link>
+            <div>
+                <button @click="switchTheme" class="hover:bg-slate-700 size-6 grid place-content-center  rounded-full hover:outline outline-1 outline-white">
+                    <font-awesome-icon :icon="['fas', 'circle-half-stroke']" />
+                </button>
+            </div>
         </nav>
     </header>
-    <main>
+    <main class="p-6 mx-auto max-w-screen-lg">
         <slot />
     </main>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { setThemeOnLoad } from './theme';
 
 library.add(fas, far, fab)
 
@@ -32,3 +33,5 @@ createInertiaApp({
     delay: 250,
   },
 })
+
+setThemeOnLoad()
