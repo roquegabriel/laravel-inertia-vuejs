8f8e68e25b0220fc9ec536105da62a683bdcb672
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-12-06 14:51:49 -0300

    Add components

---
+++ resources/js/Components/ErrorMessages.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    errors: Object,
+})
+</script>
+<template>
+    <div class="mb-4" v-if="Object.keys(errors).length">
+        <p class="text-sm text-red-500">Something went wrong!</p>
+        <ul class="ml-4 list-disc list-inside">
+            <li class="text-sm text-red-500" v-for="(error, i) in errors" key="i">{{ error }}</li>
+        </ul>
+    </div>
+</template>
