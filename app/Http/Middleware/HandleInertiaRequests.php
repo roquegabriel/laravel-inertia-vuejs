7b22c232747bb81317a6567912fc4e59f91ea60a
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-12-06 19:10:01 -0300

    Update shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,6 +37,11 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             //
+            'auth' => [
+                'user' => fn () => $request->user()
+                ? $request->user()->only('id', 'name')
+                : null,
+            ]
         ]);
     }
 }
