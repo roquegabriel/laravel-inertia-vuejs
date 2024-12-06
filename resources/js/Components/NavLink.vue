f10528350337616a7195cb281c26ec9b52ff4e32
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-12-06 11:03:36 -0300

    Create Nav Link component

---
+++ resources/js/Components/NavLink.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    routeName: String,
+})
+</script>
+
+<template>
+    <Link :href="route(routeName)" class="px-3 py-2 rounded-lg hover:bg-slate-700">
+    <slot/>
+
+    </Link>
+
+</template>
