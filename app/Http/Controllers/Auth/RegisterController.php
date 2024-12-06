ada4068a1fa22e93c9dea16c616f89c1f4ae7229
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-12-06 14:55:39 -0300

    Include registration functionality

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class RegisterController extends Controller
+{
+    // show create form
+    public function create()
+    {
+        return inertia('Auth/Register');
+    }
+
+    //register
+    public function store(Request $request)
+    {
+        sleep(2);
+        //validate
+        $credentials = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|lowercase|email|max:255',
+            'password' => 'required|confirmed|min:3',
+        ]);
+
+        $user = User::create($credentials);
+        
+        Auth::login($user);
+
+        return redirect()->route('home');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::inertia('/', 'Home')->name('home');
+
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
