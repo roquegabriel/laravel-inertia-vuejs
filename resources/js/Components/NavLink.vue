0aa6ca56cf4ac43ed3d11b8f6312d33017447d15
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-12-06 14:50:49 -0300

    Add nav link component

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -3,13 +3,13 @@ import { Link } from '@inertiajs/vue3';
 
 defineProps({
     routeName: String,
+    componentName: String,
 })
 </script>
 
 <template>
-    <Link :href="route(routeName)" class="px-3 py-2 rounded-lg hover:bg-slate-700">
-    <slot/>
-
+    <Link :href="route(routeName)" class="px-3 py-2 rounded-lg hover:bg-slate-700"
+        :class="{ 'bg-gray-700 px-2 py-1 rounded-md': $page.component === componentName }">
+    <slot />
     </Link>
-
 </template>
--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -1,13 +1,19 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import { switchTheme } from '../theme';
+import NavLink from '../Components/NavLink.vue';
 </script>
 
 <template>
     <header class="bg-slate-800 text-white">
         <nav class="p-6 mx-auto max-w-screen-lg space-x-5 flex justify-between items-center">
-            <Link  href="/" class="text-3xl" :class="{ 'bg-gray-900 px-2 py-1 rounded-md': $page.component==='Home' }">Home</Link>
-            <div>
+            <NavLink routeName="home" componentName="Home">
+                Home
+            </NavLink>
+            <div class="flex items-center space-x-6">
+                <NavLink routeName="register" componentName="Auth/Register">
+                    Register
+                </NavLink>
                 <button @click="switchTheme" class="hover:bg-slate-700 size-6 grid place-content-center  rounded-full hover:outline outline-1 outline-white">
                     <font-awesome-icon :icon="['fas', 'circle-half-stroke']" />
                 </button>
